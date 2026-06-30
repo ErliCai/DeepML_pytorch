0d2b354c38dda9df13bd798d62136dd5a4e0a25f
ErliCai <caierli123@gmail.com>
2026-06-29 17:24:45 -0700

    add solution for problem 11

--- DeepML/0011 Solve Linear Equations using Jacobi Method/jacobi_method.py
+++ DeepML/0011 Solve Linear Equations using Jacobi Method/jacobi_method.py
@@ -9,4 +9,16 @@ def solve_jacobi(A, b, n) -> torch.Tensor:
     A_t = torch.as_tensor(A, dtype=torch.float)
     b_t = torch.as_tensor(b, dtype=torch.float)
 
-    
+    jacobi = torch.zeros_like(A_t)
+    D = [b_t[i]/A_t[i][i] for i in len(A_t)]
+
+    for i in range(len(A_t)):
+        for j in range(len(A_t)):
+            if j != i:
+                jacobi[i][j] = - A_t[i][j] / A_t[i][i]
+
+    result = torch.zeros(len(A_t))
+    for i in range(n):
+        result = jacobi @ result + D
+
+    return result.round(decimals=4)
