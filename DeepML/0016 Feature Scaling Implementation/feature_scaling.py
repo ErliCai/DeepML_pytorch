aa351ac29e3f5180d6b437649b19dae356fb073f
Erli Cai <caierli123@gmail.com>
2026-06-30 11:32:52 -0700

    add second solution for problem 16

--- DeepML/0016 Feature Scaling Implementation/feature_scaling.py
+++ DeepML/0016 Feature Scaling Implementation/feature_scaling.py
@@ -8,29 +8,37 @@ def feature_scaling(data) -> tuple[torch.Tensor, torch.Tensor]:
     """
     data_t = torch.as_tensor(data, dtype=torch.float)
 
-    ## standardization
+    # ## standardization
 
-    data_standardized = torch.empty_like(data_t)
+    # data_standardized = torch.empty_like(data_t)
 
-    for i in range(len(data_t[0])):
-        col = data_t[:,i]
-        mean, std = torch.mean(col), torch.std(col, unbiased=False)
+    # for i in range(len(data_t[0])):
+    #     col = data_t[:,i]
+    #     mean, std = torch.mean(col), torch.std(col, unbiased=False)
 
-        print(mean, std)
+    #     print(mean, std)
 
-        data_standardized[:,i] = (col- mean) /std
+    #     data_standardized[:,i] = (col- mean) /std
 
-    ## minmax
+    ## standardization method 2
 
-    data_minmax = torch.empty_like(data_t)
-    for i in range(len(data_t[0])):
-        col = data_t[:,i]
-        x_min, x_max = col.min(), col.max()
+    mean, std = torch.mean(data_t, dim = 0), torch.std(data_t, dim=0, unbiased=False)
+    data_standardized = (data_t - mean) / std
 
-        data_minmax[:,i] = (col- x_min) / (x_max - x_min)
+    # ## minmax
+
+    # data_minmax = torch.empty_like(data_t)
+    # for i in range(len(data_t[0])):
+    #     col = data_t[:,i]
+    #     x_min, x_max = col.min(), col.max()
+
+    #     data_minmax[:,i] = (col- x_min) / (x_max - x_min)
+
+    ## minmax method 2
+
+    x_min = torch.min(data_t, dim=0).values
+    x_max = torch.max(data_t, dim=0).values
+
+    data_minmax = (data_t- x_min) / (x_max - x_min)
     
     return data_standardized.round(decimals=4), data_minmax.round(decimals=4)
-
-
-# mean = torch.mean(data_t, dim=0)
-# std = torch.std(data_t, dim=0, unbiased=False)
