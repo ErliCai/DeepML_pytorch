657be98274ef0a08e7ac79992b04aa8cfe7d2d04
Erli Cai <caierli123@gmail.com>
2026-07-10 15:38:29 -0700

    Implement correlation matrix

--- DeepML/0037 Calculate Correlation Matrix/correlation_matrix.py
+++ DeepML/0037 Calculate Correlation Matrix/correlation_matrix.py
@@ -10,5 +10,19 @@ def calculate_correlation_matrix(
     Compute the correlation matrix of X (and optionally Y) using PyTorch.
     If Y is None, returns the correlation matrix of X with itself.
     """
-    # Your implementation here
-    pass
+
+    X_t = torch.tensor(X,dtype=torch.float)
+    if Y is None:
+        cov = torch.cov(X_t.T)
+        std = torch.std(X_t, dim=0)
+        return cov / torch.outer(std, std)
+    else:
+        Y_t = torch.tensor(Y, dtype=torch.float)
+
+        data = torch.cat([X_t, Y_t], dim=0)
+
+        cov = torch.cov(data.T)
+        std_X = torch.std(X_t)
+        std_Y = torch.std(Y_t)
+
+        return cov/ std_X / std_Y
